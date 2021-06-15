d3cc049b0c89c37a56b82fb582f3d08d153c5fe4
codebudy5247 <a.shekhar5247@gmail.com>
2021-06-15 22:05:47 +0530

    image/audio/video upload completed

---
+++ README.md
@@ -0,0 +1,24 @@
+> Image,Audio,Video Upload API using multer and cloudinary.
+
+## JSON returned {Image}
+```JSON
+{
+  "public_id": "ImageUploads/apple.jpg",
+  "url": "https://res.cloudinary.com/media-db/raw/upload/v1623773078/AudioUploads/apple.jpg"
+}
+```
+## JSON returned {Audio}
+```JSON
+{
+  "public_id": "AudioUploads/file_example_MP3_1MG.mp3",
+  "url": "https://res.cloudinary.com/media-db/raw/upload/v1623773435/AudioUploads/file_example_MP3_1MG.mp3"
+}
+```
+
+## JSON returned {Video}
+```JSON
+{
+  "public_id": "VideoUploads/file_example_MP4_480_1_5MG",
+  "url": "https://res.cloudinary.com/media-db/video/upload/v1623774038/VideoUploads/file_example_MP4_480_1_5MG.mp4"
+}
+```
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express =require('express')
+const express = require('express')
+const path = require('path')
+const colour = require('colors')
+const logger = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const fs = require('fs')
+const multer = require('multer')
+const cloudinary = require('cloudinary').v2
 
-const colour=require('colors')
-
-const logger =require("morgan") ;
-
-const dotenv =require("dotenv");
-const cors =require ("cors");
-
-const connectDB =require ("./config/DB");
+const connectDB = require("./DB");
 
 var app = express();
 
@@ -18,8 +19,12 @@ connectDB();
 
 //Middlewares
 app.use(logger("dev"));
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({  extended: true }));
+app.use(express.json({
+    extended: true
+}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(cors());
 
 //Test Route
@@ -27,9 +32,241 @@ app.get("/", (req, res) => {
     res.send("API is running....");
 });
 
+//Routes
+//Image Upload Route
+app.post("/image", (req, res) => {
+    // Get the file name and extension with multer
+    const storage = multer.diskStorage({
+        filename: (req, file, cb) => {
+            const fileExt = file.originalname.split(".").pop();
+            const filename = `${new Date().getTime()}.${fileExt}`;
+            cb(null, filename);
+        },
+    });
 
-//Import Routes
-//app.use("/api/recipe", require('./routes/RecipeRoute'));
+    // Filter the file to validate if it meets the required audio extension
+    const fileFilter = (req, file, cb) => {
+        const filetypes = /jpg|jpeg|png/
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+        const mimetype = filetypes.test(file.mimetype)
+
+        if (extname && mimetype) {
+            return cb(null, true)
+        } else {
+            cb({
+                    message: "Unsupported File Format",
+                },
+                false
+            );
+        }
+    };
+
+    // Set the storage, file filter and file size with multer
+    const upload = multer({
+        storage,
+        limits: {
+            fieldNameSize: 200,
+            fileSize: 1024 * 1024,
+        },
+        fileFilter,
+    }).single("image");
+
+    // upload to cloudinary
+    upload(req, res, (err) => {
+        if (err) {
+            return res.send(err);
+        }
+
+        // SEND FILE TO CLOUDINARY
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.CLOUD_API_KEY,
+            api_secret: process.env.CLOUD_API_SECRET,
+        });
+        const {
+            path
+        } = req.file; // file becomes available in req at this point
+
+        const fName = req.file.originalname.split(".")[0];
+        cloudinary.uploader.upload(
+            path, {
+                resource_type: "raw",
+                public_id: `ImageUploads/${fName}`,
+            },
+
+            // Send cloudinary response or catch error
+            (err, image) => {
+                if (err) return res.send(err);
+
+                fs.unlinkSync(path);
+                //res.send(image)
+                res.json({
+                    public_id: image.public_id,
+                    url: image.secure_url
+                })
+            }
+        );
+    });
+
+});
+
+//Audio Upload Route
+app.post("/audio", async (req, res) => {
+    // Get the file name and extension with multer
+    const storage = multer.diskStorage({
+        filename: (req, file, cb) => {
+            const fileExt = file.originalname.split(".").pop();
+            const filename = `${new Date().getTime()}.${fileExt}`;
+            cb(null, filename);
+        },
+    });
+
+    // Filter the file to validate if it meets the required audio extension
+    const fileFilter = (req, file, cb) => {
+        if (file.mimetype === "audio/mp3" || file.mimetype === "audio/mpeg") {
+            cb(null, true);
+        } else {
+            cb({
+                    message: "Unsupported File Format",
+                },
+                false
+            );
+        }
+    };
+
+    // Set the storage, file filter and file size with multer
+    const upload = multer({
+        storage,
+        limits: {
+            fieldNameSize: 200,
+            fileSize: 5 * 1024 * 1024, //5 mb
+        },
+        fileFilter,
+    }).single("audio");
+
+    // upload to cloudinary
+    upload(req, res, (err) => {
+        if (err) {
+            return res.send(err);
+        }
+
+        // SEND FILE TO CLOUDINARY
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.CLOUD_API_KEY,
+            api_secret: process.env.CLOUD_API_SECRET,
+        });
+        const {
+            path
+        } = req.file; // file becomes available in req at this point
+
+        const fName = req.file.originalname.split(".")[0];
+        cloudinary.uploader.upload(
+            path, {
+                resource_type: "raw",
+                public_id: `AudioUploads/${fName}`,
+            },
+
+            // Send cloudinary response or catch error
+            (err, audio) => {
+                if (err) return res.send(err);
+
+                fs.unlinkSync(path);
+                //res.send(audio);
+                res.json({
+                    public_id: audio.public_id,
+                    url: audio.secure_url
+                })
+            }
+        );
+    });
+});
+
+//Video Upload Route
+app.post("/video", async (req, res) => {
+    // Get the file name and extension with multer
+    const storage = multer.diskStorage({
+        filename: (req, file, cb) => {
+            const fileExt = file.originalname.split(".").pop();
+            const filename = `${new Date().getTime()}.${fileExt}`;
+            cb(null, filename);
+        },
+    });
+
+    // Filter the file to validate if it meets the required video extension
+    const fileFilter = (req, file, cb) => {
+        if (file.mimetype === "video/mp4") {
+            cb(null, true);
+        } else {
+            cb({
+                    message: "Unsupported File Format",
+                },
+                false
+            );
+        }
+    };
+
+    // Set the storage, file filter and file size with multer
+    const upload = multer({
+        storage,
+        limits: {
+            fieldNameSize: 200,
+            fileSize: 30 * 1024 * 1024,
+        },
+        fileFilter,
+    }).single("video");
+
+    upload(req, res, (err) => {
+        if (err) {
+            return res.send(err);
+        }
+
+        // SEND FILE TO CLOUDINARY
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.CLOUD_API_KEY,
+            api_secret: process.env.CLOUD_API_SECRET,
+        });
+        const {
+            path
+        } = req.file; // file becomes available in req at this point
+
+        const fName = req.file.originalname.split(".")[0];
+        cloudinary.uploader.upload(
+            path, {
+                resource_type: "video",
+                public_id: `VideoUploads/${fName}`,
+                chunk_size: 6000000,
+                eager: [{
+                        width: 300,
+                        height: 300,
+                        crop: "pad",
+                        audio_codec: "none",
+                    },
+                    {
+                        width: 160,
+                        height: 100,
+                        crop: "crop",
+                        gravity: "south",
+                        audio_codec: "none",
+                    },
+                ],
+            },
+
+            // Send cloudinary response or catch error
+            (err, video) => {
+                if (err) return res.send(err);
+
+                fs.unlinkSync(path);
+                return res.json({
+                    public_id: video.public_id,
+                    url: video.secure_url
+                });
+            }
+        );
+    });
+});
+
 
 
 const PORT = process.env.PORT || 5000;
